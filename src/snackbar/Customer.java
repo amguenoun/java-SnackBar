5d06596674ec8274abc19c0910f86e97278d9778
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-30 15:40:09 -0400

    created customer class

---
+++ src/snackbar/Customer.java
@@ -0,0 +1,43 @@
+package snackbar;
+
+public class Customer{
+    private static int maxId = 0;
+    private int id;
+    private String name;
+    private double cashOnHand;
+
+    public Customer(String name, double cashOnHand){
+        maxId++;
+        id = maxId;
+        this.name = name;
+        this.cashOnHand = cashOnHand;
+    } 
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getCashOnHand() {
+        return cashOnHand;
+    }
+
+    public void setCashOnHand(double cashOnHand){
+        this.cashOnHand = cashOnHand;
+    }
+
+    public void addCash(double cash){
+        cashOnHand += cash;
+    }
+
+    public void pay(double price){
+        cashOnHand -= price;
+    }
+}
